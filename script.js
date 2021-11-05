db3dbccf39d1be33d23314fe6ac67c3c8570bffb
Inajara Pereira <inajara.souza.pereira@gmail.com>
2021-11-05 12:50:58 -0300

    adicionando style.css

--- script.js
+++ script.js
@@ -47,11 +47,15 @@ form.addEventListener('submit', handleFormSubmit);
 // é o parametro da funcao
 const addTransactionIntoDOM = transaction => {
     const li = document.createElement('li') //<li></li>
+    // condicao na linha if ternário
+    const operator = transaction.amount < 0 ? '-' : '+';
+    const amountWithoutOperator = Math.abs(transaction.amount);
+    const CssClass = transaction.amount < 0 ? 'minu' : 'plus';
 
     li.innerHTML = `
                 ${transaction.name}
-                <span> R$ ${transaction.amount}  </span>
-                <button onClick="removeTransaction(${transaction.id})">x</button>`
+                <span> ${operator} R$ ${amountWithoutOperator} </span>
+                <button class="delete-btn" onClick="removeTransaction(${transaction.id})">X</button> `
     //atribuindo um nó para o li
     transactionsUl.append(li);
 }
