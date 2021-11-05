0d7e46d22777e78d270024daa6d76ef945b307f3
Inajara Pereira <inajara.souza.pereira@gmail.com>
2021-11-05 10:59:28 -0300

    função updateBalanceValues

--- script.js
+++ script.js
@@ -1,47 +1,45 @@
 const transactionsUl = document.querySelector('#transactions');
 const inputTransactionName = document.querySelector('#text');
-const inputTransactionAmount= document.querySelector('#amount');
+const inputTransactionAmount = document.querySelector('#amount');
 
-const addTransactionIntoArray = (inputTransactionName, inputTransactionAmount) => {
-    dummyTransactions.push({
-        id: 123,
-        name: inputTransactionName,
-        amount: inputTransactionAmount
-    })
-}
+console.log(inputTransactionName);
+console.log(inputTransactionAmount);
+
+// objeto ficticio
+const dummyTransactions = [
+    {id: 1, name: 'Torta de limão', amount: -20},    
+    {id: 2, name: 'Salário', amount: 100000},    
+    {id: 3, name: 'Torta de frango com catupri', amount: -100},    
+    {id: 4, name: 'PC Gamer', amount: -8000}    
+]
 
 const handleFormSubmit = event =>{
-    event.preventDefault();
-    if(inputTransactionName.value.trim() === '' || 
-                inputTransactionAmount.value.trim() === ''){
-      alert('Informe a descrição e o valor da transação');
-      return
-    }
-    addTransactionIntoArray(inputTransactionName, inputTransactionAmount);
+    if(inputTransactionName.value.trim() === '' || inputTransactionAmount.value.trim() === ''){
+        alert('Informe a descrição e o valor da transação');       
+    }    
 }
 
 form.addEventListener('submit', handleFormSubmit);
 
-///////////////////////////////////////////////////
-
-let dummyTransactions = [
-    { id: 1, name: 'Bolo de brigadeiro', amount: -20 },
-    { id: 2, name: 'Salario', amount: 300 },
-    { id: 3, name: 'Torta de frango', amount: -10 },
-    { id: 4, name: 'Violão', amount: 150 }
-]
-
-                            // é o parametro da funcao
-const addTransactionIntoDOM = transaction =>{
+const addTransactionDOM = transaction => {
     const li = document.createElement('li')
-
     li.innerHTML = `${transaction.name}`
-    //atribuindo um nó para o li
+    //atribuindo um nó ao li
     transactionsUl.append(li);
 }
 
-const init = () =>{
-    dummyTransactions.forEach(addTransactionIntoDOM);
+const updateBalanceValues = () => {
+    const inputTransactionAmounts = dummyTransactions.map(({ amount }) => amount)
+    console.log('Somente os valores : ' + inputTransactionAmounts);
+
+    const total = inputTransactionAmounts.reduce((accumulator, transaction) => accumulator + transaction, 0);
+    console.log('Somente os valores positivos : ' + income);
+
 }
 
-init();
+
+const init = () =>{
+    dummyTransactions.forEach(addTransactionDOM);
+}
+
+init(); 
