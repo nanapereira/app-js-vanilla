472b408b5a579d28485f6b4cb7136a646e48ae52
Inajara Pereira <inajara.souza.pereira@gmail.com>
2021-11-05 09:57:28 -0300

    Adicionando note

--- script.js
+++ script.js
@@ -2,20 +2,29 @@ const transactionsUl = document.querySelector('#transactions');
 const inputTransactionName = document.querySelector('#text');
 const inputTransactionAmount= document.querySelector('#amount');
 
-const handleFormSubmit = event =>{
+const addTransactionIntoArray = (inputTransactionName, inputTransactionAmount) => {
+    dummyTransactions.push({
+        id: 123,
+        name: inputTransactionName,
+        amount: inputTransactionAmount
+    })
+}
 
+const handleFormSubmit = event =>{
+    event.preventDefault();
     if(inputTransactionName.value.trim() === '' || 
                 inputTransactionAmount.value.trim() === ''){
       alert('Informe a descrição e o valor da transação');
-
+      return
     }
+    addTransactionIntoArray(inputTransactionName, inputTransactionAmount);
 }
 
 form.addEventListener('submit', handleFormSubmit);
 
 ///////////////////////////////////////////////////
 
-const dummyTransactions = [
+let dummyTransactions = [
     { id: 1, name: 'Bolo de brigadeiro', amount: -20 },
     { id: 2, name: 'Salario', amount: 300 },
     { id: 3, name: 'Torta de frango', amount: -10 },
@@ -26,7 +35,7 @@ const dummyTransactions = [
 const addTransactionIntoDOM = transaction =>{
     const li = document.createElement('li')
 
-    li.innerHTML = ${transaction.name}
+    li.innerHTML = `${transaction.name}`
     //atribuindo um nó para o li
     transactionsUl.append(li);
 }
