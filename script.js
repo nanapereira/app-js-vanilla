1f40641871b85d0d915a40059d20360ae8b4af3b
Inajara Pereira <inajara.souza.pereira@gmail.com>
2021-11-05 12:26:17 -0300

    função removeTRansaction

--- script.js
+++ script.js
@@ -1,45 +1,99 @@
 const transactionsUl = document.querySelector('#transactions');
 const inputTransactionName = document.querySelector('#text');
 const inputTransactionAmount = document.querySelector('#amount');
+const incomeDisplay = document.querySelector('#money-plus'); // id do paragrafo que exibe as receitas
+const expenseDisplay = document.querySelector('#money-minus');// id do paragrafo que exibe as despesas
+const balanceDisplay = document.querySelector('#balance');// id do paragrafo que exibe o saldo total
 
-console.log(inputTransactionName);
-console.log(inputTransactionAmount);
-
-// objeto ficticio
-const dummyTransactions = [
-    {id: 1, name: 'Torta de limão', amount: -20},    
-    {id: 2, name: 'Salário', amount: 100000},    
-    {id: 3, name: 'Torta de frango com catupri', amount: -100},    
-    {id: 4, name: 'PC Gamer', amount: -8000}    
+// objeto literal FICTICIO
+let dummyTransactions = [
+    { id: 1, name: 'Bolo de brigadeiro', amount: -20 },
+    { id: 2, name: 'Salario', amount: 300 },
+    { id: 3, name: 'Torta de frango', amount: -10 },
+    { id: 4, name: 'Violão', amount: 150 }
 ]
 
-const handleFormSubmit = event =>{
-    if(inputTransactionName.value.trim() === '' || inputTransactionAmount.value.trim() === ''){
-        alert('Informe a descrição e o valor da transação');       
-    }    
+const geradorID = () => Math.round(Math.random() * 1000);
+
+const addTransactionInArray = (transactionName, transactionAmount) => {
+    dummyTransactions.push({
+        id: 123, // AINDA ESTA FIXO PQ NAO TEMOS UM GERADOR DE ID
+        name: transactionName,
+        amount: Number(transactionAmount)
+    })
 }
+
+const handleFormSubmit = event => {
+    event.preventDefault();
+
+    if (inputTransactionName.value.trim() === '' ||
+        inputTransactionAmount.value.trim() === '') {
+        alert('Informe a descrição e o valor da transação');
+        return;
+    }
+    addTransactionInArray(inputTransactionName.value, inputTransactionAmount.value);
+    init();
+}
+
+const removeTransaction = ID => {
+    dummyTransactions = dummyTransactions.filter(transaction => transaction.id !== ID);
+    console.log(dummyTransactions);
+    init();
+}
+
 
 form.addEventListener('submit', handleFormSubmit);
 
-const addTransactionDOM = transaction => {
-    const li = document.createElement('li')
-    li.innerHTML = `${transaction.name}`
-    //atribuindo um nó ao li
+// é o parametro da funcao
+const addTransactionIntoDOM = transaction => {
+    const li = document.createElement('li') //<li></li>
+
+    li.innerHTML = `
+                ${transaction.name}
+                <span> R$ ${transaction.amount}  </span>
+                <button onClick="removeTransaction(${transaction.id})">x</button>`
+    //atribuindo um nó para o li
     transactionsUl.append(li);
 }
 
+// metodo que irá separar os valores de total, receitas e despesas
 const updateBalanceValues = () => {
-    const inputTransactionAmounts = dummyTransactions.map(({ amount }) => amount)
-    console.log('Somente os valores : ' + inputTransactionAmounts);
 
-    const total = inputTransactionAmounts.reduce((accumulator, transaction) => accumulator + transaction, 0);
+    // pega todos so valores(amount) de cada linha do array
+    const transactionsAmounts = dummyTransactions.map(({ amount }) => amount)
+    console.log('Somente os valores : ' + transactionsAmounts);
+
+    // totalizador 
+    const total = transactionsAmounts
+        .reduce((accumulator, transaction) => accumulator + transaction, 0);
+    console.log('Soma dos valores : ' + total);
+
+    // somente as receitas
+    const income = transactionsAmounts
+        .filter(value => value > 0)
+        .reduce((accumulator, transaction) => accumulator + transaction, 0);
+
     console.log('Somente os valores positivos : ' + income);
 
+    // somente as despesas
+    const expenses = transactionsAmounts
+        .filter(value => value < 0)
+        .reduce((accumulator, transaction) => accumulator + transaction, 0);
+    console.log('Somente os valores negativos : ' + expenses);
+
+    balanceDisplay.textContent = `R$ ${total}`;
+    incomeDisplay.textContent = `R$ ${income}`;
+    expenseDisplay.textContent = `R$ ${expenses}`;
 }
 
-
-const init = () =>{
-    dummyTransactions.forEach(addTransactionDOM);
+const init = () => {
+    // aqui iremos fazer um tratamento a nivel de codigo para nao 
+    // submeter toda a lista novamente (gambiarra)
+    // solucao a nivel de codigo
+    transactionsUl.innerHTML = '';
+    dummyTransactions.forEach(addTransactionIntoDOM);
+    updateBalanceValues();
 }
 
-init(); 
+// funcao de inicializacao do js
+init();
